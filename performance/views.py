992529961f38263452f9aed3815712f6cf1cbec6
Khrystyna Sen <anollina@ukr.net>
2025-05-14 17:33:15 +0300

    Added views to apps

--- performance/views.py
+++ performance/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from rest_framework import mixins, viewsets
+from rest_framework.viewsets import GenericViewSet
+from django.db.models import F, Count
+from performance.models import TheaterHall, Performance, Reservation
+from performance.serializers import (
+    TheaterHallSerializer,
+    PerformanceSerializer,
+    PerformanceListSerializer,
+    PerformanceDetailSerializer,
+    ReservationSerializer,
+    ReservationListSerializer,
+)
+
+
+class TheaterHallViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = TheaterHall.objects.all()
+    serializer_class = TheaterHallSerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Performance.objects.all()
+        .select_related("play","theater_hall")
+        .annotate(
+            tickets_available=(
+                    F("theater_hall__rows")
+                    * F("theater_hall__seats_in_row")
+                    - Count("tickets")
+            )
+        )
+    )
+    serializer_class = PerformanceSerializer
+
+    def get_queryset(self):
+        date = self.request.query_params.get("date")
+        play_id_str = self.request.query_params.get("play")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(show_time__date=date)
+
+        if play_id_str:
+            queryset = queryset.filter(play_id=int(play_id_str))
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PerformanceListSerializer
+
+        if self.action == "retrieve":
+            return PerformanceDetailSerializer
+
+        return PerformanceSerializer
+
+
+class ReservationViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = Reservation.objects.prefetch_related(
+        "tickets__performance__play",
+        "tickets__performance__theater_hall",
+    )
+    serializer_class = ReservationSerializer
+
+    def get_queryset(self):
+        return Reservation.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ReservationListSerializer
+        return ReservationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- theater/views.py
+++ theater/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from theater.models import Genre, Actor, Play
+from theater.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    PlaySerializer,
+    PlayListSerializer,
+    PlayDetailSerializer,
+    PlayImageSerializer
+)
 
-# Create your views here.
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorsViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class PlayViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = Play.objects.prefetch_related("genres", "actors")
+    serializer_class = PlaySerializer
+
+    @staticmethod
+    def _param_to_ints(qs):
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_ids = self._param_to_ints(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        if actors:
+            actors_ids = self._param_to_ints(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+
+        if self.action == "upload_image":
+            return PlayImageSerializer
+
+        return PlaySerializer
