bc73b534fd6e6a44b88d5dae1bcfd8e9f63af4ff
Khrystyna Sen <anollina@ukr.net>
2025-05-14 19:40:27 +0300

    Added static and media settings

--- theater_api/urls.py
+++ theater_api/urls.py
@@ -18,10 +18,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/performance/", include("performance.urls", namespace="performance")),
     path("api/theater", include("theater.urls", namespace="theater")),
     path("api/user", include("actor.urls", namespace="user")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
